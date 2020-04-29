9d69e81eb313a7283870101eaf51ba3a9727cffe
seddie95 <59086410+seddie95@users.noreply.github.com>
2020-04-29 19:33:10 +0100

    Change angle using cent
    change angle of end of line using the cent

--- tuner.py
+++ tuner.py
@@ -26,7 +26,7 @@ ax.yaxis.set_ticks_position('none')
 
 # set origin and length of line
 x, y = (0, 0)
-length = 4
+length = 5
 
 # Find the end point
 endy = length * math.sin(math.radians(90))
@@ -45,14 +45,13 @@ plt.title(f"Chord: ", color='green', fontsize=20)
 arc = Arc((0, 0), 10.5, 10.5, theta1=30, theta2=150, color='b', lw=3)
 ax.add_artist(arc)
 
-# Add labels for arc
+# Add labels for arc 50Hz
 plt.text(-5.5, 2.5, "-50Hz", color='g', fontsize=18, rotation=70)
 plt.text(4.5, 2.5, "+50Hz", color='g', fontsize=18, rotation=-70)
 
 # draw circle for 0Hz
-circle = plt.Circle((0, 5.5), 0.1,  color='r')
+circle = plt.Circle((0, 5.5), 0.1, color='r')
 ax.add_artist(circle)
-
 
 
 def update_line(i, instrument):
@@ -67,39 +66,39 @@ def update_line(i, instrument):
     nearest = min(instrument.frequencies, key=lambda num: abs(num - freq))
     pitch = instrument.frequencies[nearest]
 
-    # get difference between  nearest and actual frequency
-    diff = round((freq - nearest) * -1, 2)
-
     # Display frequencies less than A4 and louder than -50 Decibels
     if decibel > -50 and freq < 440:
 
-        # update line position using frequency
-        # Show  +- 50Hz  from 30° to 150° ,(90°-30°)/50
-        pos = diff * 1.2 + 90
+        # find the semitone and multiply by 100 to get the cents
+        cent = round(((12 / math.log(2)) * math.log(nearest / freq)), 2)*100
 
-        # find the updated end of the line coordinates
-        x2 = 5 * math.cos(math.radians(pos))
-        y2 = 5 * math.sin(math.radians(pos))
+        # Set the angle using cent
+        pos = round((cent * 1.2 + 90))
 
-        # Update the lines end coordinates
-        line.set_xdata([0, x2])
-        line.set_ydata([0, y2])
+        if 30 < pos < 150:
 
-        # Set the title text and design
-        plt.title(f"Chord: {pitch},{diff}", color='green', fontsize=20)
+            # find the updated end of the line coordinates
+            x2 = 5 * math.cos(math.radians(pos))
+            y2 = 5 * math.sin(math.radians(pos))
 
+            # Update the lines end coordinates
+            line.set_xdata([0, x2])
+            line.set_ydata([0, y2])
 
-        # Change line colour depending on proximity to desired note
-        if abs(diff) < 1:
-            line.set_color('green')
+            # Set the title text and design
+            plt.title(f"Chord: {pitch},{cent:.2}", color='green', fontsize=20)
 
-        elif 3 > abs(diff) > 1:
-            line.set_color('yellow')
+            # Change line colour depending on proximity to desired note
+            if abs(cent) < 5:
+                line.set_color('green')
 
-        else:
-            line.set_color('red')
+            elif 20 > abs(cent) > 5:
+                line.set_color('yellow')
 
-        return line,
+            else:
+                line.set_color('red')
+
+            return line,
 
 
 if __name__ == "__main__":
@@ -109,6 +108,6 @@ if __name__ == "__main__":
     root.wm_title("Guitar Tuner")
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.get_tk_widget().grid(column=0, row=1)
-    animation = FuncAnimation(fig, func=update_line, frames=np.arange(0, 10, 0.1), interval=200, fargs=[guitar])
+    animation = FuncAnimation(fig, func=update_line, frames=np.arange(0, 10, 0.1), interval=500, fargs=[guitar])
 
     tk.mainloop()
